ed49da6149692e0316aae1aacfaff2f281de3ca3
Catherine Nevin <cat_nevin@hotmail.com>
2023-05-30 22:02:31 +0100

    Create controllers and routes. Successfully tested

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,46 @@
 class BooksController < ApplicationController
+    def index
+        @books = Book.all
+
+        render json: @books
+    end
+
+    def show
+        @book = Book.find(params[:id])
+        
+        render json: @book
+    end
+
+    def new
+        @book =  Book.new
+
+        render json: @book
+    end
+
+    def create
+
+        @book = Book.create(params.require(:book).permit(:title, :author, :genre, :publishedOn, :current, :meeting_date, :meeting_location, :user_id))
+
+        render json: @book
+    end
+
+    def edit
+        @book = Book.find(params[:id])
+
+        render json: @book
+    end
+
+    def update
+        @book = Book.find(params[:id])
+        @book.update(params.require(:book).permit(:title, :author, :genre, :publishedOn, :current, :meeting_date, :meeting_location))
+        
+            render json: @book
+    end
+
+    def destroy
+        @book = Book.find(params[:id])
+        @book.destroy
+
+        render json: @book
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,17 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
-  # get "users", to "users#index"
-  # get "books", to "books#index"
-  # get "books/:id", to "books#show", as: :book
+  get "users", to: "users#index"
+  get "users/:id", to: "users#show", as: :user
+  get "books", to: "books#index"
+  get "books/:id", to: "books#show", as: :book
 
+  post "users", to: "users#create"
+  post "books", to: "books#create"
 
+  patch "users/:id", to: "users#update"
+  patch "books/:id", to: "books#update"
+
+  delete "users/:id", to: "users#destroy"
+  delete "books/:id", to: "books#destroy"
 end
