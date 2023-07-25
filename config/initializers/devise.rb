64edf79d2d60f28db02db5fe1dd1805f34bd0fef
Catherine Nevin <cat_nevin@hotmail.com>
2023-07-25 12:20:00 +0100

    Update devise.rb file for secret key base

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -316,7 +316,8 @@ Devise.setup do |config|
   # config.sign_in_after_change_password = true
 
   config.jwt do |jwt|
-    jwt.secret = Rails.application.credentials.fetch(:secret_key_base)
+    # jwt.secret = Rails.application.credentials.fetch(:secret_key_base)
+    jwt.secret = ENV['SECRET_KEY_BASE']
     jwt.dispatch_requests = [
       ['POST', %r{^/login$}]
     ]
