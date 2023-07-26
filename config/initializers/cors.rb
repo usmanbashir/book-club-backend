f76ebd1e49316f68e78f8e8819541d0f9c0eb86e
Catherine Nevin <cat_nevin@hotmail.com>
2023-07-26 13:13:21 +0100

    Add cors.rb file.

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,11 +7,11 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:3000"
+    origins "https://main--jazzy-panda-bbb54e.netlify.app/"
 
     resource "*",
       headers: :any,
       expose: ['access-token', 'expiry', 'token-type', 'Authorization'],
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+      methods: [:get, :post, :put, :patch, :delete]
   end
 end
