d23756bff4b859a12e22a1ce147a5a0f0d834c35
Catherine Nevin <cat_nevin@hotmail.com>
2023-07-26 13:19:41 +0100

    Change origins link address.

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "https://main--jazzy-panda-bbb54e.netlify.app/"
+    origins "https://hello-readers.netlify.app/"
 
     resource "*",
       headers: :any,
