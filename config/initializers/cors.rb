41678673b0023b965e572fa555388a87bcb941ca
Usman Bashir <me@usmanbashir.com>
2023-08-30 21:20:36 +0300

    Add missing Authorization expose option for CORS

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -25,6 +25,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins '*'
-    resource '*', headers: :any, methods: :any
+    resource '*', headers: :any, methods: :any, expose: ['access-token', 'expiry', 'token-type', 'Authorization']
   end
 end
